54efd9ab27889330bc4579ad2b04c77cbee7fe94
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-07 17:25:27 -0600

    Ejercicio 10.13: la mutación del formulario de inicio de sesión -Completado

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,6 +3,7 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import FormikTextInput from "./FormikTextInput";
 import Text from "./Text";
+import useSignIn from "../hooks/useSignIn";
 
 const styles = StyleSheet.create({
   container: {
@@ -34,8 +35,17 @@ const initialValues = {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
