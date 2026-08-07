92529ee1a7a3628e8a39e98fa37cba7ec9d6bded
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-07 16:48:49 -0600

    Ejercicio 10.11: obtención de repositorios con Apollo Client -Completado

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
+import { ApolloProvider } from "@apollo/client";
 import Main from "./src/components/Main";
+import createApolloClient from "./src/utils/apolloClient";
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <NativeRouter>
-      <StatusBar style="auto" />
-      <Main />
+      <ApolloProvider client={apolloClient}>
+        <StatusBar style="auto" />
+        <Main />
+      </ApolloProvider>
     </NativeRouter>
   );
 }
